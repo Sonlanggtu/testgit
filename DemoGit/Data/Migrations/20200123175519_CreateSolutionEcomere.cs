d1cd5dbef75d036776afa2696283c82129497a8d
Sonlanggtu <sonlanggtu@gmail.com>
2020-01-24 01:17:14 +0700

    Create Slolution

---
+++ DemoGit/Data/Migrations/20200123175519_CreateSolutionEcomere.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DemoGit.Data.Migrations
+{
+    public partial class CreateSolutionEcomere : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
